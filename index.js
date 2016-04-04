f1d022d0d6ab8a789040d28b9b2daedc1a4f6f34
Katy Moe <katy@katy.moe>
2016-04-04 17:48:27 +0100

    initial auth route shit

--- index.js
+++ index.js
@@ -1,10 +1,55 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+var config = require('./config.json');
 
-var port = process.env.PORT;
+var twilioClient = require('twilio')(config.accountSid, config.authToken);
+
+var port = process.env.PORT || 1337;
+
+app.use(bodyParser.json());
+
+
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
+});
+
+
+var pendingAuthRequest;
+
+// GET /auth
+// sends request to katy to perform auth procedure
+app.get('/auth', function(req, res) {
+  // if (!req.secure) {
+  //   res.send({
+  //     error: 'use httpsplz'
+  //   });
+  // }
+
+  if (req.hostname !== 'localhost') {
+    res.send({
+      error: 'just localhost for now'
+    });
+  }
+
+  twilioClient.messages.create({
+    to: config.phoneNumbers.katy,
+    from: config.phoneNumbers.self,
+    body: 'NFC auth request from  ' + req.hostname,
+  }, function(error, message) {
+    if (error) {
+      res.send({
+        error: error
+      });
+    }
+    console.log('Twilio message sent: ' + message.sid);
+  });
+
+  res.send({
+    error: null,
+    message: 'successful request, katy'
+  });
 });
 
 app.listen(port, function () {
